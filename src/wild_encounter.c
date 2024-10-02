ad831ac1d0927b86c271c2b36c24efc1a32a54cb
Jonny Millar <jonny.millar29@gmail.com>
2024-10-02 21:24:14 +0100

    bugfixes (but not all bugs)

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -369,8 +369,10 @@ static u16 GetCurrentMapWildMonHeaderId(void)
             gWildMonHeaders[i].mapNum == gSaveBlock1Ptr->location.mapNum)
         {
             if (gSaveBlock1Ptr->location.mapGroup == MAP_GROUP(ROUTE104) &&
-+                gSaveBlock1Ptr->location.mapNum == MAP_NUM(ROUTE104))
-+                i += VarGet(VAR_ROUTE104NORTH_WILD_SET);
+                gSaveBlock1Ptr->location.mapNum == MAP_NUM(ROUTE104))
+            {
+                i += VarGet(VAR_ROUTE104_TRIGGERS);
+            }
             if (gSaveBlock1Ptr->location.mapGroup == MAP_GROUP(ALTERING_CAVE) &&
                 gSaveBlock1Ptr->location.mapNum == MAP_NUM(ALTERING_CAVE))
             {
